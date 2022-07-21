2e373ab5c5742ee84487a96b6f17714b2e3e20ae
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-07-21 11:02:07 +0300

    lab complete

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,12 @@
 import React from "react";
-
-function CategoryFilter() {
+import Button from "./Button";
+function CategoryFilter({categories, onFilter}) {
+  const CatBtn = categories.map(cat => (<Button key={cat} onFilter = {onFilter} cat = {cat}/>))
+  
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {CatBtn}
     </div>
   );
 }
